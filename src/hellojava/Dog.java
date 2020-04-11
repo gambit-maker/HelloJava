f05e270628bd10150345c4d435c997ed4de8979f
HP <HP@DESKTOP-AF6UDPR>
2020-04-11 22:44:12 +0700

    V1.1

---
+++ src/hellojava/Dog.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellojava;
+
+/**
+ *
+ * @author HP
+ */
+public class Dog {
+    public void sound(){
+        System.out.println("Dog Say wan wan...");
+    }
+}
